8a08ef28d22fcfa162505d1c378458eacf0a6693
schukark <schukark2004@gmail.com>
2023-10-28 23:42:35 +0300

    added SkipListNode without functionality

--- src/Data_structures/skip_list_node.cpp
+++ src/Data_structures/skip_list_node.cpp
@@ -2,8 +2,17 @@
 
 #ifndef SKIP_LIST_NODE_CPP
 
+template<class T>
 class SkipListNode {
+public:
+    SkipListNode(T&& value): value(value), next(nullptr), level_below(nullptr) {}
 
+private:
+    T value;
+    SkipListNode* next;
+    SkipListNode* level_below;
 };
+
+
 
 #endif
